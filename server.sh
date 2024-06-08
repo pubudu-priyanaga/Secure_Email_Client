81c4e49446db24208adb2ea912319cdabfab2674
Pubudu Priyanga <99382658+pubudu-priyanaga@users.noreply.github.com>
2024-06-08 11:39:46 +0530

    Delete server.sh

--- server.sh
+++
@@ -1,7 +0,0 @@
-echo "[+] Adding PYTHONPATH..."
-export PYTHONPATH=`pwd`:$PYTHONPATH
-echo "[+] Starting webpymail server..."
-cd webpymail
-python3 manage.py runserver
-cd ..
-echo "[+] Server closed..."

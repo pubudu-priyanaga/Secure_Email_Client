d62fd791a343e943cfcbe4352fc4098caadbc5c9
Pubudu Priyanga <99382658+pubudu-priyanaga@users.noreply.github.com>
2024-06-08 11:41:02 +0530

    Delete prepare.sh

--- prepare.sh
+++
@@ -1,7 +0,0 @@
-echo "[+] Adding PYTHONPATH..."
-export PYTHONPATH=`pwd`:$PYTHONPATH
-echo "[+] Run migrate --run-syncdb..."
-cd webpymail
-python3 manage.py migrate --run-syncdb
-cd ..
-echo "[+] Initialization done..."
